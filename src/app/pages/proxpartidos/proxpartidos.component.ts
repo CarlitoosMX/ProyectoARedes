075782b42673c842614ed1de6315124e23a8825e
Robusto <50712323+RBautista1292@users.noreply.github.com>
2023-12-07 04:03:27 -0600

    cxbcx

--- src/app/pages/proxpartidos/proxpartidos.component.ts
+++ src/app/pages/proxpartidos/proxpartidos.component.ts
@@ -8,6 +8,7 @@ import { ApiService, EventResponse } from '../../service/api.service';
 export class ProxpartidosComponent implements OnInit {
   // Usa la interfaz para definir el tipo de datos
   ppartidos: any;
+  prediccion: any;
 
   constructor(private apiService: ApiService) {}
 
@@ -24,17 +25,18 @@ export class ProxpartidosComponent implements OnInit {
     });
   }
 
-  prediction(){
+  prediction(equipo1: string, equipo2: string){
     const predictionData = {
       "model": "gpt-3.5-turbo",
       "messages": [
         {"role": "system", "content": "Eres un analista de futbol dedicado a analizar el rendimiento de los equipos."},
-        {"role": "user", "content": "Los equipos de la Liga Mexicana MX siguientes van a jugar un partido, y estas son algunas estadísticas pasadas de sus partidos anteriores: FC América: Últimos partidos jugados: 2-0, ganador, 2-1, ganador, 3-0, perdedor; León: Últimos partidos jugados: 3-1, perdedor, 1-0, ganador, 0-0, empate. Analiza el rendimiento de ambos equipos y determina quién tiene más posibilidades de salir victorioso del encuentro."}
+        {"role": "user", "content": `Los equipos de la Liga Mexicana MX siguientes van a jugar un partido, y estas son algunas estadísticas pasadas de sus partidos anteriores: ${equipo1}: Últimos partidos jugados: 2-0, ganador, 2-1, ganador, 3-0, perdedor; ${equipo2}: Últimos partidos jugados: 3-1, perdedor, 1-0, ganador, 0-0, empate. Analiza el rendimiento de ambos equipos y determina quién tiene más posibilidades de salir victorioso del encuentro.`}
       ]
     };
 
     this.apiService.requestPrediction(predictionData).subscribe((response) => {
       console.log(response.choices[0].message.content);
+      this.prediccion = response.choices[0].message.content;
     });
   }
 }
