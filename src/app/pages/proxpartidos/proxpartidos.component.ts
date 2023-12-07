813c610f4435f5c8ee31748ed2e966c7386cfff2
Robusto <50712323+RBautista1292@users.noreply.github.com>
2023-12-07 01:24:35 -0600

    agadg

--- src/app/pages/proxpartidos/proxpartidos.component.ts
+++ src/app/pages/proxpartidos/proxpartidos.component.ts
@@ -7,19 +7,34 @@ import { ApiService, EventResponse } from '../../service/api.service';
 })
 export class ProxpartidosComponent implements OnInit {
   // Usa la interfaz para definir el tipo de datos
-  ppartidos: EventResponse[] = [];
+  ppartidos: any;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.partidos();
+    //this.prediction();
   }
 
   partidos() {
-    this.apiService.getLastEvents('133602').subscribe((data) => {
+    this.apiService.getLastEvents().subscribe((data) => {
       // Asigna la propiedad específica del objeto JSON que esperas recibir
-      this.ppartidos = data;
+      this.ppartidos = data.events;
       console.log(this.ppartidos);
+    });
+  }
+
+  prediction(){
+    const predictionData = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {"role": "system", "content": "Eres un analista de futbol dedicado a analizar el rendimiento de los equipos."},
+        {"role": "user", "content": "Los equipos de la Liga Mexicana MX siguientes van a jugar un partido, y estas son algunas estadísticas pasadas de sus partidos anteriores: FC América: Últimos partidos jugados: 2-0, ganador, 2-1, ganador, 3-0, perdedor; León: Últimos partidos jugados: 3-1, perdedor, 1-0, ganador, 0-0, empate. Analiza el rendimiento de ambos equipos y determina quién tiene más posibilidades de salir victorioso del encuentro."}
+      ]
+    };
+
+    this.apiService.requestPrediction(predictionData).subscribe((response) => {
+      console.log(response.choices[0].message.content);
     });
   }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 // Define una interfaz que represente la estructura del objeto JSON que esperas recibir
@@ -13,12 +13,42 @@ export interface EventResponse {
 })
 export class ApiService {
 
-  private urlApi2 = 'https://thesportsdb.com/api/v1/json/3/eventslast.php';
+  //private urlApi2 = 'https://thesportsdb.com/api/v1/json/3/eventslast.php';
 
   constructor(private http: HttpClient) { }
 
-  public getLastEvents(teamId: string): Observable<any> {
-    const url = `${this.urlApi2}?id=${teamId}`;
+  public getLastEvents(): Observable<any> {
+    const url = 'https://thesportsdb.com/api/v1/json/3/eventsround.php?id=4350&r=17&s=2023-2024';
     return this.http.get<any>(url);
+  }
+
+  public getTeams(): Observable<any> {
+    const url = 'http://34.125.186.179:3000/data/equipos';
+    return this.http.get<any>(url);
+  }
+
+  public getResults(): Observable<any> {
+    const url = 'http://34.125.243.118:3000/data/resultados';
+    return this.http.get<any>(url);
+  }
+
+  public getPlayers(): Observable<any> {
+    const url = 'http://34.125.186.179:3000/data/jugadores';
+    return this.http.get<any>(url);
+  }
+
+  public getPredictions(): Observable<any> {
+    const url = 'http://34.125.243.118:3000/data/predicciones';
+    return this.http.get<any>(url);
+  }
+
+  public getUsers(): Observable<any> {
+    const url = 'http://34.125.186.179:3000/data/usuarios';
+    return this.http.get<any>(url);
+  }
+
+  public requestPrediction(body: any): Observable<any> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json', 'Authorization': 'Bearer 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'});
+    return this.http.post<any>('https://api.openai.com/v1/chat/completions', body, { headers });
   }
 }
